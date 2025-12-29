add83c0cebe3177b14c32e117b64684d13a2a752
Shivam Kumar <bhardwajshivam026@gmail.com>
2025-12-29 22:36:54 +0530

    Added message , workspace and channel schema

---
+++ src/schema/message.schema.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema(
+  {
+    body: {
+      type: String,
+      required: [true, 'Message body is required']
+    },
+    image: {
+      type: String
+    },
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'SenderId is required']
+    },
+    channelId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Channel',
+      required: [true, 'ChannelId is required']
+    },
+    workspaceId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Workspace',
+      required: [true, 'WorkspaceId is required']
+    }
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model('Message', messageSchema);
+
+export default Message;
---
+++ src/schema/workspace.schema.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const workspaceSchema = new mongoose.Schema(
+  {
+    //name , members , channels , joinCode ,
+    name: {
+      type: String,
+      required: [true, 'workspace name is required'],
+      unique: true
+    },
+    description: {
+      type: String
+    },
+    memebers: [
+      {
+        memberId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User'
+        },
+        role: {
+          type: String,
+          enum: ['admin', 'member'],
+          default: 'member'
+        }
+      }
+    ],
+    channels: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Channel'
+      }
+    ],
+    joinCode: {
+      type: String,
+      required: [true, 'Join code is required']
+    }
+  },
+  { timestamps: true }
+);
+
+const Workspace = mongoose.model('Workspace', workspaceSchema);
+
+export default Workspace;
