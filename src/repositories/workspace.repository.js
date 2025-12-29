6998b48e2f5ac833a9d8ccfc82afb6e0445dbcfd
Shivam Kumar <bhardwajshivam026@gmail.com>
2025-12-29 23:40:38 +0530

    Added all workspace repository layer functions

---
+++ src/repositories/workspace.repository.js
@@ -0,0 +1,123 @@
+import crudRepository from "./crudRepository.js";
+import Workspace from "../schema/workspace.schema.js";
+import ClientError from "../utils/errors/clientErros.js";
+import { StatusCodes } from "http-status-codes";
+import User from "../schema/user.schema.js";
+import Channel from "../schema/channel.schema.js";
+
+
+const workspaceRepository = {
+    ...crudRepository(Workspace),
+    getWorkspaceByName : async function(workspaceName){
+        const workspace = await Workspace.findOne({
+            name : workspaceName
+        })
+        if(!workspace){
+            throw new ClientError({
+                message : 'workspace not found',
+                explanation : 'Invalid data sent from the client',
+                statusCode : StatusCodes.NOT_FOUND
+            })
+        }
+        return workspace;
+    },
+    getWorkspaceByJoinCode : async function(workspaceJoincode){
+        const workspace = await Workspace.findOne({
+            joincode : workspaceJoincode,
+        })
+        if(!workspace){
+            throw new ClientError({
+                message : 'workspace not found',
+                explanation : 'Invalid data sent from the client',
+                statusCode : StatusCodes.NOT_FOUND
+            })
+        }
+        return workspace;
+    },
+    addMemberToWorkspace : async function(workspaceId,memberId,role){
+        const workspace = await Workspace.findById(workspaceId).populate('members');
+
+        if(!workspace){
+            throw new ClientError({
+                message : 'workspace not found',
+                explanation : 'Invalid data sent from the client',
+                statusCode : StatusCodes.NOT_FOUND
+            })
+        }
+
+        const user = await User.findById(memberId);
+        if(!user){
+            throw new ClientError({
+                message : 'user not found',
+                explanation : 'Invalid data sent from the client',
+                statusCode : StatusCodes.NOT_FOUND
+            })
+        }
+
+        const isAlreadyPresentInWorkspace = workspace.memebers.find(
+            (member) => member.memberId === memberId
+        )
+
+        if(isAlreadyPresentInWorkspace){
+            throw new ClientError({
+                message : 'user already in workspace',
+                explanation : 'Invalid user sent from client',
+                statusCode : StatusCodes.FORBIDDEN
+            })
+        }
+
+        workspace.memebers.push({
+            memberId : memberId,
+            role : role
+        })
+
+        await workspace.save();
+
+        return workspace;
+    },
+    addChannelToWorkspace : async function(workspaceId,channelName){
+        const workspace = await Workspace.findById(workspaceId).populate('channels');
+
+        if(!workspace){
+            throw new ClientError({
+                message : 'workspace not found',
+                explanation : 'Invalid data sent from the client',
+                statusCode : StatusCodes.NOT_FOUND
+            })
+        }
+
+        const isChannelAlreadyPresentInWorkspace = workspace.channels.find(
+            (channel)=> channel.name === channelName
+        )
+
+        if(isChannelAlreadyPresentInWorkspace){
+            throw new ClientError({
+                message : 'channel already present in workspace',
+                explanation : 'Invalid data sent from the client',
+                statusCode : StatusCodes.FORBIDDEN
+            })
+        }
+
+        const channel = await Channel.create({
+            name : channelName,
+            workspaceId : workspaceId,
+        })
+
+        workspace.channels.push({
+            channel,
+        })
+
+        await workspace.save();
+
+        return workspace;
+    },
+    getAllWorkspaceByMemberId : async function(memberId){
+        const workspaces = await Workspace.find({
+            'members.memberId' : memberId
+        }).populate('members.memberId','username email avatar');
+
+        return workspaces;
+    }
+}
+
+export default workspaceRepository;
